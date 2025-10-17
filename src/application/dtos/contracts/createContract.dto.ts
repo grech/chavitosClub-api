732724a5bf20a92c94e2b72f38756db24fed4a4d
Memin <gestradach@gmail.com>
2025-10-17 17:47:23 -0600

    chore: add validation on DTO to lowers permited eventDate and add dump BD

--- src/application/dtos/contracts/createContract.dto.ts
+++ src/application/dtos/contracts/createContract.dto.ts
@@ -17,7 +17,6 @@ export class CreateContractDto {
 
           const {name,phone,email,rfc='NA', eventDate, address, hours, services} = object;
 
-          if(!name || name.legth < 3) return ['El nombre es obligatorio']
           if(!phone) return ['el telefono debe ser obligatorio']
           if(!email) return ['el email es obligatorio']
           if(!eventDate) return ['la fecha es obligatoria']
@@ -28,6 +27,13 @@ export class CreateContractDto {
           const hoursN = Number(hours);
           if(!hoursN || isNaN(hoursN) || hoursN < 1) return ['las horas deben ser validas']
           if(!services || !Array.isArray(services)) return ['Debes elegir al menos un servicio']
+          
+          const eventDaten = new Date(eventDate);
+          const today = new Date();
+          const towddays = 2*24*60*60*1000;
+          const allowedDate = new Date(today.getTime() + towddays);
+          if(eventDaten < allowedDate) return ['la fecha del evento tiene que ser futura']
+          if(!name || name.legth < 3) return ['El nombre es obligatorio']
 
           return [undefined, new CreateContractDto(name, phone, email, rfc, eventDate, address, hoursN, services)]
 
